666c6bdcad52e378a026533dbd876df5de9ce5e7
Maxson Henrique Silveira Alves <maxsonhenrique@Mac-Pro-de-Maxson.local>
2025-01-19 17:48:05 -0300

    sdsdsd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function updateTimer() {
-    const startDate = new Date("2022-02-22T00:00:00"); // Data de início
+    const startDate = new Date("2022-02-22T00:00:00");
     const now = new Date();
     const diff = now - startDate;
 
@@ -12,6 +12,61 @@ function updateTimer() {
 
     document.getElementById("timer").innerHTML = 
         `${years} anos, ${months} meses, ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+    
+    requestAnimationFrame(updateTimer); // Requisita a próxima atualização
 }
 
-setInterval(updateTimer, 1000);
+requestAnimationFrame(updateTimer); // Inicia o cronômetro
+
+
+const photo = document.getElementById("photo");
+const photos = [
+    "img/foto1.png",
+    "img/foto2.png",
+    "img/foto3.png",
+    "img/foto4.png",
+    "img/foto5.png"
+];
+let currentPhotoIndex = 0;
+
+photo.addEventListener("click", () => {
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+    photo.src = photos[currentPhotoIndex];
+});
+photo.addEventListener("click", () => {
+    photo.style.opacity = 0.5; // Torna a imagem semi-transparente
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+    photo.src = photos[currentPhotoIndex];
+    photo.onload = () => {
+        photo.style.opacity = 1; // Restaura a opacidade quando a nova imagem carregar
+    };
+});
+function typeMessage() {
+    const message = "Você é a melhor coisa que me aconteceu!";
+    let index = 0;
+    const element = document.getElementById("message");
+    const interval = setInterval(() => {
+        element.innerHTML += message[index];
+        index++;
+        if (index === message.length) {
+            clearInterval(interval);
+        }
+    }, 100);
+}
+document.getElementById("musicButton").addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/watch?app=desktop&v=izGwDsrQ1eQ";
+});
+function typeMessage() {
+    const message = "Você é a melhor coisa que me aconteceu!";
+    let index = 0;
+    const element = document.getElementById("message");
+    const interval = setInterval(() => {
+        element.innerHTML += message[index];
+        index++;
+        if (index === message.length) {
+            clearInterval(interval);
+        }
+    }, 100);
+}
+
+window.onload = typeMessage;
